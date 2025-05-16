b0d4d62161981068eb8c697b26a1e40d8fbafc4a
RamonBaarroso <ramondiasbarroso@gmail.com>
2025-05-16 18:04:37 -0300

    Add files via upload

---
+++ ProductCatalog.Infrastructure/Services/JsonCacheService.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using ProductCatalog.Core.Interfaces;
+using ProductCatalog.Core.Services;
+using ProductCatalog.Core.Exceptions;
+
+namespace ProductCatalog.Infrastructure.Services
+{
+  public class JsonCacheService : IJsonCacheService
+  {
+    private readonly string _filePath;
+
+    public JsonCacheService()
+    {
+      var folder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+      _filePath = Path.Combine(folder, "favorites_cache.json");
+    }
+
+    /// <summary>
+    /// Função de salvar favoritos
+    /// </summary>
+    /// <param name="productIds"></param>
+    /// <returns></returns>
+    /// <exception cref="CacheException"></exception>
+    public async Task SaveFavoritesAsync(List<int> productIds)
+    {
+      try
+      {
+        var json = JsonSerializer.Serialize(productIds, new JsonSerializerOptions { WriteIndented = true });
+        await File.WriteAllTextAsync(_filePath, json);
+      }
+      catch (Exception ex)
+      {
+        throw new CacheException("Falha ao salvar favoritos.", ex);
+      }
+    }
+
+    /// <summary>
+    /// Função de carregar favoritos salvos em cache
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="CacheException"></exception>
+    public async Task<List<int>> LoadFavoritesAsync()
+    {
+      try
+      {
+        if (!File.Exists(_filePath))
+          return new List<int>();
+
+        var json = await File.ReadAllTextAsync(_filePath);
+
+        return JsonSerializer.Deserialize<List<int>>(json) ?? new List<int>();
+      }
+      catch (Exception ex)
+      {
+        throw new CacheException("Falha ao carregar favoritos", ex);
+      }
+    }
+  }
+}
---
+++ ProductCatalog.Infrastructure/Services/ProductService.cs
@@ -0,0 +1,121 @@
+﻿using System.Net.Http.Json;
+using ProductCatalog.Core.DTOs;
+using ProductCatalog.Core.Interfaces;
+using Polly;
+using Polly.Retry;
+using ProductCatalog.Core.Exceptions;
+using Polly.Extensions.Http;
+using ProductCatalog.Core.Domain.Entities;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ProductCatalog.Infrastructure.Services
+{
+
+  public class ProductService : IProductService
+  {
+    private readonly HttpClient _httpClient;
+    private readonly AsyncRetryPolicy<HttpResponseMessage> _retryPolicy;
+
+    public ProductService(HttpClient httpClient)
+    {
+      _httpClient = httpClient;
+
+      _retryPolicy = HttpPolicyExtensions
+          .HandleTransientHttpError()
+          .WaitAndRetryAsync(3, retryAttempt =>
+              TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+    }
+
+    /// <summary>
+    /// Busca todos os produtos da API
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="ProductServiceException"></exception>
+    public async Task<IEnumerable<ProductListDto>> GetAllAsync()
+    {
+      try
+      {
+        var response = await _retryPolicy.ExecuteAsync(() => _httpClient.GetAsync(""));
+
+        if (!response.IsSuccessStatusCode)
+        {
+          throw new ProductServiceException($"Falha ao carregar produtos.");
+        }
+
+        var products = await response.Content.ReadFromJsonAsync<List<ProductListDto>>();
+        return products ?? new List<ProductListDto>();
+      }
+
+      catch (HttpRequestException ex)
+      {
+        throw new ProductServiceException("Erro de rede ao buscar produtos. Verifique sua conexão.", ex);
+      }
+
+      catch (Exception ex)
+      {
+        throw new ProductServiceException("Ocorreu um erro inesperado ao buscar produtos", ex);
+      }
+    }
+
+    /// <summary>
+    /// Traz os detalhes do produto
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    /// <exception cref="ProductServiceException"></exception>
+    public async Task<ProductDetailDto?> GetDetailsByIdAsync(int id)
+    {
+      try
+      {
+        var response = await _retryPolicy.ExecuteAsync(() => _httpClient.GetAsync($"{id}"));
+
+        if (!response.IsSuccessStatusCode)
+        {
+          throw new ProductServiceException($"Falha ao carregar os detalhes do produto para o ID {id}. Código de status: {response.StatusCode}");
+        }
+
+        var product = await response.Content.ReadFromJsonAsync<ProductDetailDto>();
+        return product;
+      }
+      catch (HttpRequestException ex)
+      {
+        throw new ProductServiceException($"Erro de rede ao buscar detalhes do produto para o ID {id}. Verifique sua conexão.", ex);
+      }
+      catch (Exception ex)
+      {
+        throw new ProductServiceException($"Ocorreu um erro inesperado ao buscar detalhes do produto para o ID { id}.", ex);
+      }
+    }
+
+    /// <summary>
+    /// Busca o produto favoritado pelo usuario 
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    /// <exception cref="ProductServiceException"></exception>
+    public async Task<FavoriteProductDto?> GetFavoriteByIdAsync(int id)
+    {
+      try
+      {
+        var response = await _retryPolicy.ExecuteAsync(() => _httpClient.GetAsync($"{id}"));
+
+        if (!response.IsSuccessStatusCode)
+        {
+          throw new ProductServiceException($"Falha ao carregar os detalhes do produto para o ID {id}. Código de status: {response.StatusCode}");
+        }
+
+        var product = await response.Content.ReadFromJsonAsync<FavoriteProductDto>();
+        return product;
+      }
+      catch (HttpRequestException ex)
+      {
+        throw new ProductServiceException($"Erro de rede ao buscar detalhes do produto para o ID {id}. Verifique sua conexão.", ex);
+      }
+      catch (Exception ex)
+      {
+        throw new ProductServiceException($"Ocorreu um erro inesperado ao buscar detalhes do produto para o ID {id}.", ex);
+      }
+    }
+
+  }
+}
